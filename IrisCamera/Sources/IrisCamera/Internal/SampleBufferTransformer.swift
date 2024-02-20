19f9d6d0466ce961cabba8b0e7f1d2938c5f0332
Patrick Lin <patricksclin@gmail.com>
2024-02-20 19:29:57 +0700

    Added Color invert filter

--- IrisCamera/Sources/IrisCamera/Internal/SampleBufferTransformer.swift
+++ IrisCamera/Sources/IrisCamera/Internal/SampleBufferTransformer.swift
@@ -1,16 +1,26 @@
 import AVKit
 import CoreImage
+import CoreImage.CIFilterBuiltins
 import Foundation
 
 struct SampleBufferTransformer {
+    let ciContext = CIContext()
+
     func transform(videoSampleBuffer: CMSampleBuffer) -> CMSampleBuffer {
         guard let pixelBuffer = CMSampleBufferGetImageBuffer(videoSampleBuffer) else {
             print("failed to get pixel buffer")
             fatalError()
         }
 
-        #warning("Implementation to invert colors in pixel buffer")
+        let filter = CIFilter.colorInvert()
+        filter.inputImage = CIImage(cvPixelBuffer: pixelBuffer)
 
+        guard let filteredImage = filter.outputImage else {
+            print("failed to get filtered image")
+            fatalError()
+        }
+
+        ciContext.render(filteredImage, to: pixelBuffer)
 
         guard let result = try? pixelBuffer.mapToSampleBuffer(timestamp: videoSampleBuffer.presentationTimeStamp) else {
             fatalError()
